b8d739ce65f641141cb4813ca545cff0cc0aa429
Ethan Chen <intervigil@gmail.com>
2020-09-17 21:59:41 +0200

    certus: Use Soong namespace
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/certus/certus-vendor.mk)
