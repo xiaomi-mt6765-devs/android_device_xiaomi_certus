1c19e3eaa256973f23c44acb17af920a26947e95
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0530

    certus: Migrate to Health 2.1 HAL
    Change-Id: I960c7367b6dc2e1bd11b739ecd6216d65f281588
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -21,6 +21,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
