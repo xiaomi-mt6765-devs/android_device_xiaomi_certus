a9a8d5d6be8450d52960b26c9aa3cab3b6781338
Aayush Gupta <aayushgupta219@gmail.com>
2021-01-19 11:27:45 +0530

    certus: Label light HIDL and grant required permissions
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ifc68e9ee59f9ca259b4e88ff382373900e1c455d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ VENDOR_SECURITY_PATCH := 2019-03-05
 # SEPolicy
 TARGET_USING_LEGACY_SELINUX := true
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
