cfd4aff1aadee7dfb437a059be9a051b5264c678
Vaisakh Murali <mvaisakh@statixos.com>
2021-04-25 18:10:36 +0530

    certus: Build libhwc2onfbadapter and libhwc2on1adapter
    Stock has these for display and hwc dependency
    
    Change-Id: I4e90a37dfb2ec132ccc8c58e47d13e2b43ed3de5
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -70,6 +70,11 @@ PRODUCT_PACKAGES += \
     libdrmclearkeyplugin \
     libmockdrmcryptoplugin
 
+# Display
+PRODUCT_PACKAGES += \
+    libhwc2on1adapter \
+    libhwc2onfbadapter
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
