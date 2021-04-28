caa4c2b944f179bab2d0bf93126a43631508e14a
Kshitij Gupta <kshitijgm@gmail.com>
2021-04-28 03:27:34 +0200

    certus: Copy MediaTek privapp permissions
    Change-Id: If71a5d42f68b39b3c1c77a040ffd80e74686068b

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     ImsInit
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-org.lineageos.xiaomi.imsinit.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-org.lineageos.xiaomi.imsinit.xml
 
 # Keylayout
