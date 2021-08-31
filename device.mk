33c5d508537863b3d6d5887414091528935d79a0
Debayan <kardebayan3@gmail.com>
2021-08-31 10:10:28 +0530

    certus: Build Thermal HIDLs from source
    Change-Id: I3cf1ca237a44eac3966b1cc47dae058ed763798a
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -146,6 +146,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
