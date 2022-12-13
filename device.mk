cca0261953cea848f5644a2b6769a2148fe2f975
SamarV-121 <samarvispute121@pm.me>
2022-12-13 12:54:07 +0530

    certus: Link older libutils for android.hardware.audio@5.0-service-mediatek
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'

--- device.mk
+++ device.mk
@@ -255,6 +255,7 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcompiler_rt.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcompiler_rt.so
 
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v30.so \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
 
 PRODUCT_COPY_FILES += \
