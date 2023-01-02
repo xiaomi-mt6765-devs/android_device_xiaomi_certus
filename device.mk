4b1156da0655a81759cb94efbc343fe3ffc27050
Hadenix <vsegda_online_007@bk.ru>
2023-01-03 01:28:52 +0300

    certus: Import keylayout configs from ALPS ARM64 0.4 vendor
    * Only needed keys are kept
    * https://raw.githubusercontent.com/gaganmalvi/gaganmalvi/main/patches/fix-dt2w.patch
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
