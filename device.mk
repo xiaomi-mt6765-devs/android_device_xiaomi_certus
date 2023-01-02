dfd0d4975895161c38ab6000d94cdd918271b219
Hadenix <vsegda_online_007@bk.ru>
2023-01-03 01:28:52 +0300

    certus: Import keylayout configs from ALPS ARM64 0.4 vendor
    * Only needed keys are kept
    * https://raw.githubusercontent.com/gaganmalvi/gaganmalvi/main/patches/fix-dt2w.patch
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ TARGET_SCREEN_WIDTH := 720
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
