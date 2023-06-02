ed20fd86f9ace2ecb3d3f79fb9a12a04ee865dd1
Kshitij Gupta <kshitijgm@gmail.com>
2023-06-02 19:39:18 +0200

    certus: Create required vendor symlinks for gatekeeper
    Change-Id: I0d87c2b4332c0bcff0a7872fffd13e8ec1225c6e
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- Android.mk
+++ Android.mk
@@ -9,4 +9,18 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),certus)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
