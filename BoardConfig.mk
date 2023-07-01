057ab9db9d6f7db6f070ab12807a8c35a9a41acf
Christian Oder <myself5@carbonrom.org>
2023-07-01 17:20:17 +0700

    certus: Build disabled VBMeta image
    Change-Id: I48e3ff106a2e4e044ed39b86a94f3bc80b93f530

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,5 +65,9 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_HAS_MTK_HARDWARE := true
 TARGET_BOARD_PLATFORM := mt6765
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit the proprietary files
 include vendor/xiaomi/certus/BoardConfigVendor.mk
