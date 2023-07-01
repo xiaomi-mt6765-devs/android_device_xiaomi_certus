b5173f52fc8f1628383e15a8763e6cf6d16ecc25
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-01 17:26:41 +0700

    certus: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8

--- device.mk
+++ device.mk
@@ -3,5 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/certus/certus-vendor.mk)
