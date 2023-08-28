9d0ff3512ba7076352b3bb7c7cb54dd8689565e8
Sushrut1101 <guptasushrut@gmail.com>
2023-08-28 12:21:35 +0530

    certus: Copy default cacerts from AOSP
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4.vendor \
     android.hardware.camera.provider@2.5.vendor
 
+# Certs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,system/ca-certificates/files/,$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts)
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
