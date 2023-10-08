d1dd78390d5a183f6c91ae122050256dd841d8fc
Hadenix <vsegda_online_007@bk.ru>
2023-10-08 14:56:32 +0300

    certus: Initial skeleton
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/certus
+
+# Inherit the proprietary files
+include vendor/xiaomi/certus/BoardConfigVendor.mk
