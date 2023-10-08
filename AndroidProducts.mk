4f0940347cff5271473649f575201f1c50f2b3f2
Hadenix <vsegda_online_007@bk.ru>
2023-10-08 14:56:32 +0300

    certus: Initial skeleton
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_certus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_certus-user \
+    lineage_certus-userdebug \
+    lineage_certus-eng
