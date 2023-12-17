70094e4c242d29752dbd894367170d965a133f1b
Hadenix <vsegda_online_007@bk.ru>
2023-12-17 15:19:50 +0300

    certus: Import rootdir scripts from ALPS ARM64 0.6 vendor
    * full_certus-user-10-QP1A.190711.020-mp1V91221-test-keys
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -4,5 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    factory_init.connectivity.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6765.rc \
+    init.mt6765.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.rc \
+    multi_init.rc \
+    fstab.mt6765 \
+    ueventd.mt6765.rc
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/certus/certus-vendor.mk)
