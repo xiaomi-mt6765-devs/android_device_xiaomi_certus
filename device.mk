cad61f614f4fdb69bca0a60e056460c1b957b5fa
Hadenix <vsegda_online_007@bk.ru>
2023-12-17 15:31:26 +0300

    certus: Import WiFi configs from ALPS ARM64 0.4 vendor
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -64,5 +64,11 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/certus/certus-vendor.mk)
