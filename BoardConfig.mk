5622bc0258d7bf9806aa4aec3130846d135f0685
Hadenix <vsegda_online_007@bk.ru>
2023-12-17 15:00:54 +0300

    certus: Add security patch from ALPS ARM64 0.4 vendor
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,9 @@ TARGET_USING_LEGACY_SELINUX := true
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# SPL
+VENDOR_SECURITY_PATCH := 2020-10-05
+
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
