4394bbfdaf8b540d634163da86e2ba477edff4f9
Hadenix <vsegda_online_007@bk.ru>
2023-12-20 17:14:26 +0300

    certus: Cleanup rootdir scripts
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -140,19 +140,11 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    factory_init.connectivity.rc \
-    factory_init.rc \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6765.rc \
     init.mt6765.usb.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.rc \
-    multi_init.rc \
     fstab.mt6765 \
     ueventd.mt6765.rc
 
