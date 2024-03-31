0b327069ec847d71d79c0d3ac9cb290fa27be65a
Hadenix <vsegda_online_007@bk.ru>
2024-03-31 08:51:41 +0300

    certus: Build vendor variant of android.hardware.bluetooth@1.0
    * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.bluetooth@1.0-service-mediatek": library "android.hardware.bluetooth@1.0.so" not found: needed by main executable
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.a2dp@1.0.vendor
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
