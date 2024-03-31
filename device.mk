9764f9cd05b746e8cc2ae40ec441b795897172bd
Hadenix <vsegda_online_007@bk.ru>
2024-03-31 08:50:02 +0300

    certus: Build vendor variant of android.hardware.bluetooth.a2dp@1.0
    * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.audio@5.0-service-mediatek": library "android.hardware.bluetooth.a2dp@1.0.so" not found: needed by main executable
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0.vendor \
