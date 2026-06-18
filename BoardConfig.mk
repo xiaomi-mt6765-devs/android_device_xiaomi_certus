2250ebdaff057fefe528f54a17da44d21a96a0ac
Hadenix <vsegda_online_007@bk.ru>
2026-06-18 19:28:10 +0300

    certus: Define Architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,15 @@
 
 DEVICE_PATH := device/xiaomi/certus
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/certus/BoardConfigVendor.mk
